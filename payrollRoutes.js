e6d9fd782dd446cb675b727a5cb9eea190972b0a
Manish Panda <manishpanda591@gmail.com>
2026-07-04 17:35:05 +0530

    Delete payrollRoutes.js

--- payrollRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const payrollController = require("../controllers/payrollController");
-
-router.get("/", payrollController.getPayroll);
-router.post("/", payrollController.createPayroll);
-
-module.exports = router;
