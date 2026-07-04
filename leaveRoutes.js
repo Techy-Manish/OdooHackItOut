1c41e76b3842506dcef864166c61090dfb99fd7b
chakrabortyanjista-tech06 <chakrabortyanjista@gmail.com>
2026-07-04 17:35:22 +0530

    Delete leaveRoutes.js

--- leaveRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const leaveController = require("../controllers/leaveController");
-
-router.post("/apply", leaveController.applyLeave);
-router.get("/", leaveController.getLeaves);
-router.put("/:id", leaveController.approveLeave);
-
-module.exports = router;
